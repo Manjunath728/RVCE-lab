84d2d5c5179aaa39bb2d0ff0b654fa1d0d7219ca
Manjunath B <manjunathb.mca22@rvce.edu.in>
2023-03-01 10:55:55 +0530

    march-1 lss class coverd topic

---
+++ shell-script/first.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# nessary  to tell terminal to run in bash
+
+VAR1=1234
+TEST=RVCEMCA
+
+#DOUBLE QUOTES
+echo "this is quote  $VAR1 $TEST "
+
+#SINGLE QUOTES 
+echo 'this is single quote demo $VAR1 $TEST '
+
+#REVERSE QUOTE / back tik
+echo "this computer /laptop hostname is:`TEST` "
+
+
+
+
